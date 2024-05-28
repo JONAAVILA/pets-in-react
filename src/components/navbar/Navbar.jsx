f2970a74c1205961eb850c24533d6dffcca9e9ee
JONAAVILA <jona_1969@hotmail.com>
2024-05-28 10:25:16 -0300

    nav bar animation

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,23 +5,25 @@ import { useState } from 'react';
 
 export default function Navbar (){
 
-    const [ nav, setNav ] = useState('container_nav_off')
+    const [ clas, setClas ] = useState('off')
 
     const handleNavbar = ()=>{
-        if(nav === 'container_nav_off'){
-            setNav('container_nav_activate')
+        if(clas === 'off'){
+            setClas('on')
         }else{
-            setNav('container_nav_off')
+            setClas('off')
         }
     }
     return(
-        <div className={`container_nav ${nav}`} >
-            <img className="image_nav" src={profile} alt="" />
+        <div className={`container_nav ${clas}`} >
             <div className='icon_menu2' onClick={handleNavbar} >
                 <IconMenu2 stroke={2}
-                           width={20} 
-                           color={'#525252'}
-                           />
+                        width={20} 
+                        color={'#525252'}
+                        />
+            </div>
+            <div className='box_nav' >
+                <img src={profile} alt="" />
             </div>
         </div>
     )
--- src/views/Landing.jsx
+++ src/views/Landing.jsx
@@ -5,13 +5,7 @@ export default function Landing (){
     return(
         <div className="container_landing" >
             <Navbar/>
-            <div className="box_landing" > 
-                <h1>NOBLEZA CANINA</h1>
-                <h3>¿Buscás tu mascota soñada?</h3>
-                <p>
-                Podemos ayudarte a conseguirla, en unos simples pasos podes tener tu mascota soñada
-                </p>
-            </div>
+    
         </div>
     )
 }
