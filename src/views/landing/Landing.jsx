bc597a67432620665e9f816737a595801043ed60
JONAAVILA <jona_1969@hotmail.com>
2024-06-07 09:27:07 -0300

    navbar mobile fixed

--- src/views/landing/Landing.jsx
+++ src/views/landing/Landing.jsx
@@ -20,36 +20,38 @@ export default function Landing (){
     }
 
     return(
-        <>
-        <div className="box_navbar_landing" >
-            <Navbar handleNavbar={handleNavbar} clas={clas} />
-        </div>
-        <div className="container_landing" >
-            <div className="box_landing" > 
-                <div className="box_heading" >
-                    <h1>FURRYS CACHORROS</h1>
-                </div>
-                <div className='sub_heading' >
-                    <div>
-                        <h2>¿Buscás tu mascota soñada?</h2>
-                        <p>
-                            En Furrys Cachorros, nos especializamos en la venta de mascotas de raza con pedigree, asegurando que cada uno de nuestros animales provenga de las mejores líneas de sangre y cumpla con los más altos estándares de calidad y salud.
-                        </p>
+        <div className='container_root' >
+            <div className="box_navbar_landing" >
+                <Navbar handleNavbar={handleNavbar} clas={clas} />
+            </div>
+            <div className='box_root' >
+                <div className="container_landing" >
+                    <div className="box_landing" > 
+                        <div className="box_heading" >
+                            <h1>FURRYS CACHORROS</h1>
+                        </div>
+                        <div className='sub_heading' >
+                            <div>
+                                <h2>¿Buscás tu mascota soñada?</h2>
+                                <p>
+                                    En Furrys Cachorros, nos especializamos en la venta de mascotas de raza con pedigree, asegurando que cada uno de nuestros animales provenga de las mejores líneas de sangre y cumpla con los más altos estándares de calidad y salud.
+                                </p>
+                            </div>
+                        </div>
+                        <div className='icons_landing' >
+                            <IconBrandInstagram stroke={1} width={20} />
+                            <IconBrandMeta stroke={1} width={20} />
+                            <a href="href=https://wa.link/imsfli">
+                                <IconBrandWhatsapp stroke={1} width={20} />
+                            </a>
+                        </div>
+                    </div>
+                    <div className={`circle_langing ${clas}_landing`} >
+                        <img className={`caniche ${clas}_caniche`} src={caniche} alt="caniche colorado" />
                     </div>
                 </div>
-                <div className='icons_landing' >
-                    <IconBrandInstagram stroke={1} width={20} />
-                    <IconBrandMeta stroke={1} width={20} />
-                    <a href="href=https://wa.link/imsfli">
-                        <IconBrandWhatsapp stroke={1} width={20} />
-                    </a>
-                </div>
-            </div>
-            <div className={`circle_langing ${clas}_landing`} >
-                <img className={`caniche ${clas}_caniche`} src={caniche} alt="caniche colorado" />
+                <Home clas={clas} />
             </div>
         </div>
-        <Home clas={clas} />
-        </>
     )
 }
