5f71e34ce0286ba796471e70431169df2fa28120
JONAAVILA <jona_1969@hotmail.com>
2024-06-18 12:45:13 -0300

    link social networks

--- src/views/landing/Landing.jsx
+++ src/views/landing/Landing.jsx
@@ -39,8 +39,18 @@ export default function Landing (){
                             </div>
                         </div>
                         <div className='icons_landing' >
-                            <IconBrandInstagram stroke={1} width={20} />
-                            <IconBrandMeta stroke={1} width={20} />
+                            <a href="https://www.instagram.com/furryscachorros_?utm_source=qr&igsh=aTF6dzRkNHF6bGls"
+                               target='_blank'
+                               rel='noreferrer'
+                               aria-label="Contáctanos por Instagram">
+                               <IconBrandInstagram stroke={1} width={20} />
+                            </a>
+                            <a href="https://www.facebook.com/profile.php?id=61560421724095&mibextid=qi2Omg&rdid=Rw2olpH2kyobj2U0&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2Fdj2gA4KiKE37U113%2F%3Fmibextid%3Dqi2Omg"
+                               target='_blank'
+                               rel='noreferrer'
+                               aria-label="Contáctanos por Facebook">
+                               <IconBrandMeta stroke={1} width={20} />
+                            </a>
                             <a href="https://wa.link/imsfli"
                                target='_blank'
                                rel='noreferrer'
